2ab088c2c5db5bcd293cf8c9e15f5a5f78f72fc4
pockyee <baoqi85@gmail.com>
2024-09-26 18:17:08 +0200

    finish hangman

--- Hello_Hangman.py
+++ Hello_Hangman.py
@@ -1,31 +1,61 @@
-# Write your code here
 import random
 
 print("H A N G M A N")
 
-attempts = 8
-word_list = ['python', 'java', 'swift', 'javascript']
-result = random.choice(word_list)
+won = 0
+lost = 0
 
-hint = '-' * len(result)
+while True:
+    choice = input ('Type "play" to play the game, "results" to show the scoreboard, and "exit" to quit:')
+    if choice == 'play':
+        attempts = 8
+        word_list = ['python', 'java', 'swift', 'javascript']
+        result = random.choice(word_list)
 
+        hint = '-' * len(result)
+        guessed = set()
 
-while attempts > 0:
-    x = input('\n'+hint+'\nInput a letter:')
-    if x in result:
-        indices = [index for index, letter in enumerate(result) if letter == x]
-        for index in indices:
-            temp = list (hint)
-            temp[index] = x
-            hint = ''.join(temp)
-    else:
-        print("That letter doesn't appear in the word.")
-    attempts -= 1
-        
-print('Thanks for playing!')
-# word = input('Guess the word:'+ hint)
+        while attempts > 0:
+            print(f"\n{hint}")
+            x = input("Input a letter: ")
+            
+            if len(x) != 1:
+                print('Please, input a single letter.')
+                continue
+            
+            if not x.islower() or not x.isalpha():
+                print('Please, enter a lowercase letter from the English alphabet.')
+                continue
+            
+            if x in guessed:
+                print("You've already guessed this letter.")
+                continue
 
-# if word == result:
-#     print ('You survived!')
-# else:
-#     print ('You lost!')
+            guessed.add(x)
+
+            if x in result:
+                # Update the hint by replacing dashes with the correct guessed letter
+                temp = list(hint)
+                for index, letter in enumerate(result):
+                    if letter == x:
+                        temp[index] = x
+                hint = ''.join(temp)
+                
+                if '-' not in hint:
+                    break  # Word is fully guessed
+            else:
+                print("That letter doesn't appear in the word.")
+                attempts -= 1
+
+        if '-' not in hint:
+            print(f'You guessed the word {hint}!\nYou survived!')
+            won += 1
+        else:
+            print(f'\nYou lost! The word was: {result}')
+            lost += 1
+        continue
+    elif choice == 'results':
+        print (f'You won: {won} times\nYou lost: {lost} times')
+        continue
+    elif choice == 'exit':
+        break
