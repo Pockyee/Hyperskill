db7ffd3909b6859176493b6cb7c3eb374369f083
pockyee <baoqi85@gmail.com>
2024-10-07 18:43:16 +0200

    finish

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
 import argparse
 import math
+from os import abort
+
+
+class DataTypeError(Exception):
+    def __str__(self):
+        return "No data type defined!"
+
+
+class SortingTypeError(Exception):
+    def __str__(self):
+        return "No sorting type defined!"
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-dataType", default="word")
-parser.add_argument("-sortingType", default="natural")
+parser.add_argument("-dataType", nargs="?", default="word")
+parser.add_argument("-sortingType", nargs="?", default="natural")
+parser.add_argument("-inputFile", nargs="?")
+parser.add_argument("-outputFile", nargs="?")
 
-args = parser.parse_args()
-
-
-# def mergesort_list(x):
-#     if len(x) <= 1:
-#         return x
-#     else:
-#         mid = len(x) // 2
-#         left = mergesort_list(x[:mid])
-#         right = mergesort_list(x[mid:])
-#         return merge(left, right)
-#
-#
-# def merge(left, right):
-#     new = []
-#     i = j = 0
-#     while i < len(left) and j < len(right):
-#         if left[i] < right[j]:
-#             new.append(left[i])
-#             i += 1
-#         else:
-#             new.append(right[j])
-#             j += 1
-#     new.extend(left[i:])
-#     new.extend(right[j:])
-#     return new
+try:
+    args, unknown = parser.parse_known_args()
+    assert len(unknown) == 0
+    if args.dataType is None:
+        raise DataTypeError
+    if args.sortingType is None:
+        raise SortingTypeError
+except DataTypeError as de:
+    print(de)
+    exit()
+except SortingTypeError as se:
+    print(se)
+    exit()
+except AssertionError:
+    for i in unknown:
+        print(f'"{i}" is not a valid parameter. It will be skipped.')
 
 
 def read():
@@ -38,7 +42,13 @@ def read():
         try:
             if args.dataType == "long":
                 user_input = input().split()
-                data.extend(map(int, user_input))
+                for i in user_input:
+                    try:
+                        value = int(i)
+                    except ValueError:
+                        print(f'"{i}" is not a long. It will be skipped.')
+                    else:
+                        data.append(value)
             if args.dataType == "line":
                 user_input = input()
                 data.append(user_input)
@@ -55,20 +65,55 @@ def read():
     return user_dict
 
 
+def file_read(userfile):
+    global data
+    file = open(userfile, "r")
+    if args.dataType == "long":
+        user_input = file.read().split()
+        for i in user_input:
+            try:
+                value = int(i)
+            except ValueError:
+                print(f'"{i}" is not a long. It will be skipped.')
+            else:
+                data.append(value)
+    if args.dataType == "line":
+        data = file.readlines()
+    elif args.dataType == "word":
+        user_input = file.read().split()
+        data.extend(user_input)
+    for element in data:
+        if element in user_dict:
+            user_dict[element] += 1
+        else:
+            user_dict[element] = 1
+    return user_dict
+
+
 converter = {"long": "numbers", "word": "words", "line": "lines"}
 data = []
 user_dict = {}
-read()
-print(f"Total {converter[args.dataType]}: {len(data)}.")
+output = ""
+if args.inputFile:
+    file_read(args.inputFile)
+else:
+    read()
+output += f"Total {converter[args.dataType]}: {len(data)}."
 if args.sortingType == "natural":
     if args.dataType == "line":
-        print("Sorted data: \n" + "\n".join(map(str, sorted(data))))
+        output += "\nSorted data: \n" + "\n".join(map(str, sorted(data)))
     else:
-        print("Sorted data: "+" ".join(map(str,sorted(data))))
+        output += "\nSorted data: " + " ".join(map(str, sorted(data)))
 elif args.sortingType == "byCount":
     sorted_dict = {key: user_dict[key] for key in sorted(user_dict)}
     sorted_keys = sorted(sorted_dict, key=lambda key: user_dict[key])
     for i in sorted_keys:
-        print(f"{i}: {sorted_dict[i]} time(s), {math.floor(sorted_dict[i]/len(data)*100)}%")
+        output += f"\n{i}: {sorted_dict[i]} time(s), {math.floor(sorted_dict[i] / len(data) * 100)}%"
 else:
-    print("Error!")
+    print("unexpected Error")
+
+if args.outputFile:
+    file = open(args.outputFile, "w")
+    file.write(output)
+    file.close()
+print(output)
--- test.py
+++ test.py
@@ -1,7 +1,2 @@
-import sys
-
-def main():
-    print("Arguments:", sys.argv[1:])  # 打印所有传递的参数
-
-if __name__ == "__main__":
-    main()
+file = open("test.txt","r")
+print(file.read().split())
